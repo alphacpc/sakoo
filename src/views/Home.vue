a13866190bb20da4c3914887073bf1175a32463d
alphacpc <alphacpc@gmail.com>
2022-01-20 13:54:49 +0000

    Added data and bug when we list all product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h2>Page d'accueil</h2>
+
+    <div  v-for="product in products">
+      <p>{{product.Nom}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'Home',
-}
+  import myDatas from "./../data.json";
+
+  const products = myDatas;
+
+  console.log(products);
+
+  export default {
+    name: 'Home',
+
+  }
+
 </script>
